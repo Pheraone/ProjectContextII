ab9ff960d13d89e12f9ade859c984c52f49e7b7a
Pheraone <brechje@me.com>
2021-03-18 21:28:37 +0100

    basic movement
    (URP issues)

---
+++ Assets/Scripts/PlayerMovement/OLDBeeMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OLDBeeMovement : MonoBehaviour
+{
+    public float forwardSpeed = 25f;
+    public float strafeSpeed = 7.5f;
+    public float hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+
+    public float lookRotateSpeed = 90f;
+    public Vector2 lookInput, screenCenter, mouseDistance;
+
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    void Start()
+    {
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        //calculate mouse distance
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.x;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+
+        //rollInput = 
+
+        //TODO: not turn as much when not flying
+        transform.Rotate(-mouseDistance.y * lookRotateSpeed * Time.deltaTime, mouseDistance.x * lookRotateSpeed * Time.deltaTime, 0f, Space.Self);
+
+        //Move();
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration *  Time.deltaTime);
+
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += transform.right * activeStrafeSpeed * Time.deltaTime;
+        transform.position += transform.up * activeHoverSpeed * Time.deltaTime;
+
+    }
+
+
+
+}
